3b1cb35491d7a27f4901f105de0ef1eba74b6ac0
bofallon <bofallon@gmail.com>
2024-08-19 09:52:30 -0700

    Added code to lambda exercise

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,8 @@
 # quick demo of lambda vs functions
+pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+sorted_pairs = sorted(pairs, key=lambda x: x[1])
+
+def get_second_item(pair):
+    return pair[1]
+
+sorted_pairs = sorted(pairs, key=get_second_item)
